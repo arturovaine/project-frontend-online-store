f565cec3518b8f1b596330cc164509f72d5aa397
Daniel Dias <danieldias.b99@hotmail.com>
2021-10-25 17:12:18 -0300

    Adiciona Inputs.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
+import Categories from '../components/Categories';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+
+    this.saveCategories = this.saveCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.saveCategories();
+  }
+
+  async saveCategories() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
+        <input data-testid="query-input" type="text" />
+        <input data-testid="query-button" type="button" value="Pesquisar" />
+
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <Link to="/cart" data-testid="shopping-cart-button"> Carrinho </Link>
+        <Categories categories={ categories } />
       </div>
     );
   }
